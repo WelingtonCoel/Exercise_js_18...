a7ed85f9638b0152727913ea104b7931b155db1d
WelingtonCoel <53539574+WelingtonCoel@users.noreply.github.com>
2023-08-20 23:26:01 +0100

    Add files via upload

--- funcao04.js
+++ funcao04.js
@@ -3,8 +3,9 @@
 function fatorial(n){
     let fat = 1
     for(let c = n; c > 1; c--){
-        fat*= c
-
+        fat*= c               //(fat = 1; c=5 (fa=5)), (fat = 5 ; c=4(fat=20)
+        console.log(fat)      // (fat = 20 ; c=3(fat=60), (fat = 60 ; c=2(fat=120)
+                              // (fat = 120 ; c=1 (fat=120)
   }
 return fat 
 }
